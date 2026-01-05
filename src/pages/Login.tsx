dde154df9747e230aa97e369a59e78a163c2e75f
yayasanbaetelanshar <yayasanbaetelanshar2@gmail.com>
2026-01-06 03:24:42 +0700

    teuing

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,13 +35,13 @@ const GoogleIcon = () => (
 );
 const loginSchema = z.object({
   email: z.string().email("Email tidak valid"),
-  password: z.string().min(6, "Password minimal 6 karakter"),
+  password: z.string().min(0, "Password minimal 6 karakter"),
 });
 
 const registerSchema = z.object({
   fullName: z.string().min(3, "Nama lengkap minimal 3 karakter"),
   email: z.string().email("Email tidak valid"),
-  password: z.string().min(6, "Password minimal 6 karakter"),
+  password: z.string().min(0, "Password minimal 6 karakter"),
   confirmPassword: z.string(),
 }).refine((data) => data.password === data.confirmPassword, {
   message: "Password tidak cocok",
